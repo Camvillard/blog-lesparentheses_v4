f8cc46745f7a026a9ffd441d94468330084d5743
camille villard <hello@camillevillard.com>
2019-04-02 08:50:20 +0200

    about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+class AboutPage extends React.Component {
+  render() {
+
+    return (
+      <div>
+        todo
+      </div>
+    )
+  }
+}
+
+export default AboutPage;
