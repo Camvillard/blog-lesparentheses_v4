b4ac532a1a66e552f27fdb39ffb24ba184af7aea
camille villard <hello@camillevillard.com>
2019-04-02 08:35:36 +0200

    sass plugin

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,15 +1,14 @@
-/**
- * Layout component that queries for data
- * with Gatsby's StaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/static-query/
- */
-
+// external librairies
 import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
+// internal stuff
 import Header from "./header"
+
+
+// importing styles
+import '../styles/main.scss';
 
 const Layout = ({ children }) => (
   <StaticQuery
