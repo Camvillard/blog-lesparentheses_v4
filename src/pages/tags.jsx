7a37e0a36cbf6b241773e613902a6e708bcb755f
camille villard <hello@camillevillard.com>
2019-05-02 13:35:13 -0400

    added tags in articles

---
+++ src/pages/tags.jsx
@@ -0,0 +1,16 @@
+// external librairies
+import React from 'react';
+
+// internal stuff
+
+// styles
+
+class TagsPage extends React.Component {
+  render() {
+    return(
+      <div>todo</div>
+    )
+  }
+}
+
+export default TagsPage;
